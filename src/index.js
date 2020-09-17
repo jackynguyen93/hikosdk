2fc36dd29be6b9185147c62e4056f9593519768b
Hiko Nguyen <nguyen.vq@hikosolution.com>
2020-09-17 17:36:25 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import Register from './components/register/register.vue';
 
-export default {
-  install(Vue) {
-    Vue.component('hk-register', Register);
-  },
-};
+export function install (Vue) {
+  if (install.installed) return
+  install.installed = true
+  Vue.component('hk-register', Register);
+}
+
+export const VRegister = Register
+
+const plugin = {
+  install
+}
+
+// Auto-install
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+export default plugin
