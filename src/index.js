5b7aac41cba5f35969d86021ca869472246ee4b4
Hiko Nguyen <nguyen.vq@hikosolution.com>
2020-09-17 16:49:40 +0700

    fix build

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Register from './components/register/register.vue';
+
+export default {
+  install(Vue) {
+    Vue.component('hk-register', Register);
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname + './../src/index.js'),
+  entry: path.resolve(__dirname + '/src/index.js'),
   output: {
-    path: path.resolve(__dirname + './../dist'),
-    filename: 'v-breadcrumps.js'
+    path: path.resolve(__dirname + '/dist'),
+    filename: 'index.js'
   },
   module: {
     rules: [{
